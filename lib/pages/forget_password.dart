d34291d791affaf2076a250e65cf2b47429b54b3
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-11-17 23:46:28 +0530

    Update forget_password.dart

--- lib/pages/forget_password.dart
+++ lib/pages/forget_password.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api, avoid_unnecessary_containers
-
 import 'package:crud2/pages/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
